563aeb12ad428b5cc6a819849dbef5c1aedcccd3
20141074010339 <20141074010339@MC125524-.ifrn.local>
2018-02-08 19:38:03 -0300

    modificando

--- SisOrg/src/SisOrg/controllers/CadernoController.java
+++ SisOrg/src/SisOrg/controllers/CadernoController.java
@@ -4,33 +4,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import SisOrg.daos.CadernosDAO;
+import SisOrg.daos.UsuarioDAO;
 import SisOrg.models.Cadernos;
+import SisOrg.models.Usuario;
 
 
 public class CadernoController {
+	
 	@RequestMapping ("sistema/index")
 	public String index() {
 		
 		return "sistema/index";
 	}
 
-	@RequestMapping ("usuarios/form")
-	public String form2(){
-		System.out.println("Acessando um formulário de cadernos");
+	@RequestMapping ("cadernos/form-cadastro")
+	public String form(){
+		System.out.println("Acessando um formulário de contatos");
 		
-		return "usuarios/form";
+		return "cadernos/form-cadastro";
 	}
 	
-	@RequestMapping (value= "cadernos", method=RequestMethod.POST)
-	
-	 public String gravar (Cadernos c) {
+	 @RequestMapping (value= "usuarios", method=RequestMethod.POST)
 		
-		CadernosDAO dao = new CadernosDAO ();
-		dao.inserir(c);
+	 public String gravar (Usuario u) {
+		
+		UsuarioDAO dao = new UsuarioDAO ();
+		dao.inserir(u);
 		
 		return "sistema/home";
 		
 	}
-	
-	
 }
